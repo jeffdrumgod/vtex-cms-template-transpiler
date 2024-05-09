d1c51f0c16ffd480e2bce9b2f495c992b4732cc4
Jefferson Rafael Kozerski <jeff.drumgod@gmail.com>
2024-05-09 07:35:27 -0300

    fix: update cert

--- src/certs/generate.sh
+++ src/certs/generate.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
 cat > config_openssl.cnf <<EOF
-[dn]
-CN = vtexlocal.online.pro.br
-[req]
+[ req ]
+default_bits = 2048
+prompt = no
+default_md = sha256
+x509_extensions = v3_req
 distinguished_name = dn
-[EXT]
-subjectAltName = DNS:vtexlocal.online.pro.br
-keyUsage = digitalSignature
-extendedKeyUsage = serverAuth
+
+[ dn ]
+C = BR
+ST = YourState
+L = YourCity
+O = YourOrganization
+OU = YourOrganizationalUnit
+CN = *.vtexlocal.online.pro.br
+
+[ v3_req ]
+basicConstraints = CA:FALSE
+keyUsage = nonRepudiation, digitalSignature, keyEncipherment
+subjectAltName = @alt_names
+
+[ alt_names ]
+DNS.1 = *.vtexlocal.online.pro.br
+DNS.2 = vtexlocal.online.pro.br
 EOF
 
-openssl req -x509 -out selfsigned.crt -keyout selfsigned.key \
-  -newkey rsa:2048 -nodes -sha256 \
-  -days 358000 \
-  -subj '/CN=vtexlocal.online.pro.br' \
-  -extensions EXT -config config_openssl.cnf
+openssl req -x509 -nodes -days 358000 -newkey rsa:2048 -keyout selfsigned.key -out selfsigned.crt -config config_openssl.cnf
 
 rm config_openssl.cnf
