da3483c808b451d10b4f89dda06a91883ee9cc46
Jefferson Rafael Kozerski <jeff.drumgod@gmail.com>
2024-05-09 07:29:19 -0300

    fix: update cert

--- src/certs/generate.sh
+++ src/certs/generate.sh
@@ -1,4 +1,20 @@
+#!/bin/bash
+
+cat > config_openssl.cnf <<EOF
+[dn]
+CN = vtexlocal.online.pro.br
+[req]
+distinguished_name = dn
+[EXT]
+subjectAltName = DNS:vtexlocal.online.pro.br
+keyUsage = digitalSignature
+extendedKeyUsage = serverAuth
+EOF
+
 openssl req -x509 -out selfsigned.crt -keyout selfsigned.key \
   -newkey rsa:2048 -nodes -sha256 \
+  -days 358000 \
   -subj '/CN=vtexlocal.online.pro.br' \
-  -extensions EXT -config <(printf "[dn]\nCN=vtexlocal.online.pro.br\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:vtexlocal.online.pro.br\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
+  -extensions EXT -config config_openssl.cnf
+
+rm config_openssl.cnf
